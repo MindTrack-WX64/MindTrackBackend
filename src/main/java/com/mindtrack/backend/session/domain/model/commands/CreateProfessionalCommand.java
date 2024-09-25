3270d1f3d6e2f8f21f3d9640a43906460d1eb495
JP19-03 <u202014511@upc.edu.pe>
2024-09-25 16:09:58 -0500

    refactor(clinicalHistory): change CreateProfessionalCommand to receive Professional attributes.

--- src/main/java/com/mindtrack/backend/session/domain/model/commands/CreateProfessionalCommand.java
+++ src/main/java/com/mindtrack/backend/session/domain/model/commands/CreateProfessionalCommand.java
@@ -1,7 +1,12 @@
 package com.mindtrack.backend.session.domain.model.commands;
 
+import java.time.LocalDate;
+
 public record CreateProfessionalCommand(
         String professionalType,
-        Long profileId
+        String fullName,
+        String email,
+        String phone,
+        LocalDate birthDate
 ) {
 }

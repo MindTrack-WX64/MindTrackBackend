09631822a37c71b5291dd1c90bb1e2a460a703d7
JP19-03 <u202014511@upc.edu.pe>
2024-09-26 11:09:27 -0500

    refactor(treatmentPlan): undo removing of date in BiologicalFunction constructor.

--- src/main/java/com/mindtrack/backend/treatmentPlan/domain/model/commands/AddBiologicalFunctionCommand.java
+++ src/main/java/com/mindtrack/backend/treatmentPlan/domain/model/commands/AddBiologicalFunctionCommand.java
@@ -1,6 +1,9 @@
 package com.mindtrack.backend.treatmentPlan.domain.model.commands;
 
+import java.time.LocalDate;
+
 public record AddBiologicalFunctionCommand(
+        LocalDate date,
         int hunger,
         int sleep,
         int hydration,
--- src/main/java/com/mindtrack/backend/treatmentPlan/domain/model/entities/BiologicalFunction.java
+++ src/main/java/com/mindtrack/backend/treatmentPlan/domain/model/entities/BiologicalFunction.java
@@ -22,11 +22,14 @@ public class BiologicalFunction {
         this.energy = 0;
     }
 
-    public BiologicalFunction(int hunger, int sleep, int hydration, int energy) {
+    public BiologicalFunction(LocalDate date, int hunger, int sleep, int hydration, int energy) {
+        if (date == null) {
+            throw new IllegalArgumentException("Date must be non-null");
+        }
         if (hunger < 0 || sleep < 0 || hydration < 0 || energy < 0) {
             throw new IllegalArgumentException("Biological values must be non-negative");
         }
-        this.date = LocalDate.now();
+        this.date = date;
         this.hunger = hunger;
         this.sleep = sleep;
         this.hydration = hydration;
